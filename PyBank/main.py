46312570a3ff51ffe6fb1790afb24b0a1e1c15c8
Arrow <46056460+tanjowen@users.noreply.github.com>
2019-03-10 23:13:13 -0700

    pybankv2

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,12 +12,12 @@ max_ = 0
 min_ = 0
 month_count = 0
 total = 0
+monthly_change = []
 
 with open(csvpath, newline="") as csvfile:
 	csvreader = csv.reader(csvfile, delimiter=",")
 	csv_header =next(csvreader)
-	print('Financial Analysis')
-	print('-------------------------')
+	
 	for money in csvreader:
 		month = money[0]
 		monthly_bal = int(money[1])
@@ -31,9 +31,31 @@ with open(csvpath, newline="") as csvfile:
 		preset = monthly_bal
 		month_count = month_count + 1
 		total = total + monthly_bal
+		monthly_change.append(difference)
 
+	average = (sum(monthly_change)-monthly_change[1])/ (len(monthly_change)-1)
+
+
+
+
+print('Financial Analysis')
+print('-------------------------')
 print(f'Total Months: {month_count}')
 print(f'Total: ${total}')
-print(f'Average Change:')
+print(f'Average Change: ${round(average, 2)}')
 print(f'Greatest Increase in Profits: {max_date} (${max_})')
 print(f'Greatest Decrease in Profits: {min_date} (${min_})')
+
+output_path = os.path.join("Financial Analysis.csv")
+
+with open(output_path, "w", newline="") as csvfile:
+	new = csv.writer(csvfile, delimiter=',')
+
+	new.writerow(['Financial Anlysis'])
+	new.writerow(['----------------------------'])
+	new.writerow([f'Total Months: {month_count}'])
+	new.writerow([f'Total: ${total}'])
+	new.writerow([f'Average Change: ${round(average, 2)}'])
+	new.writerow([f'Greatest Increase in Profits: {max_date} (${max_})'])
+	new.writerow([f'Greatest Decrease in Profits: {min_date} (${min_})'])
+
